910a17fdb20ba12bdf524d2c3cf40366c92f26da
Abhishek shukla <abhishekshukla0700@gmail.com>
2021-05-14 03:11:10 +0530

    removed credential details

--- customer/settings.py
+++ customer/settings.py
@@ -139,8 +139,8 @@ CRISPY_TEMPLATE_PACK ='bootstrap4'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yantraimmigration2@gmail.com'
-EMAIL_HOST_PASSWORD = 'biiaxlfnfarhtrxw'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'YantraEducare <noreply@yantraeducare.com>'
 
